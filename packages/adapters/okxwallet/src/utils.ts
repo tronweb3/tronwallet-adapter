9e4bc1f9685ddfd8e7314184bd62f36f01efa8a5
Zorro <zorro11639@gmail.com>
2025-03-10 11:16:13 +0800

    feat(okxwallet,bybit,gatewalet): confirm window.navigator exists tu support SSR

--- packages/adapters/okxwallet/src/utils.ts
+++ packages/adapters/okxwallet/src/utils.ts
@@ -4,10 +4,15 @@ export function supportOkxWallet() {
     return !!(window.okxwallet && window.okxwallet.tronLink);
 }
 
-export const isOKApp = /OKApp/i.test(navigator.userAgent);
-
+export const isOKApp = typeof navigator !== 'undefined' && /OKApp/i.test(navigator.userAgent);
+export function isInOKApp() {
+    if (typeof window !== 'undefined' && typeof window.navigator !== 'undefined') {
+        return /OKApp/i.test(window.navigator.userAgent);
+    }
+    return false;
+}
 export function openOkxWallet() {
-    if (!isOKApp && isInMobileBrowser()) {
+    if (!isInOKApp() && isInMobileBrowser()) {
         window.location.href = 'okx://wallet/dapp/url?dappUrl=' + encodeURIComponent(window.location.href);
         return true;
     }
