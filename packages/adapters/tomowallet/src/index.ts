4a433cefcec88720269c16f1d7b3f864bc006959
fleksin <hezhoufan@gmail.com>
2025-05-08 15:10:33 +0800

    chore: code cleanup and unit test

--- packages/adapters/tomowallet/src/index.ts
+++ packages/adapters/tomowallet/src/index.ts
@@ -1,3 +1,1 @@
 export * from './adapter.js';
-// export * from './types.js';
-// export * from './utils.js';
--- packages/adapters/tomowallet/tests/units/utils.ts
+++
@@ -1,9 +0,0 @@
-export async function wait() {
-    const p = new Promise((resolve) => {
-        setTimeout(resolve, 1000);
-    });
-    jest.advanceTimersByTime(1000);
-    await p;
-    await p;
-}
-export const ONE_MINUTE = 62 * 1000;
