e9fc3bf9abb9371226874e6bbf489141f7a311f7
Zorro <zorro11639@gmail.com>
2026-03-16 18:13:25 +0800

    test(binance): update jsdom to happy-dom

--- packages/adapters/binance/vite.config.ts
+++ packages/adapters/binance/vite.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         globals: true,
-        environment: 'jsdom',
-        deps: {
-            inline: ['@my-org/ts-lib'],
-        },
+        environment: 'happy-dom',
     },
 });
